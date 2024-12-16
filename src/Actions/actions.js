f6bdae93eddc46b945868ddb555b3cfa66c8432b
amit47-eng <av495267@gmail.com>
2024-12-16 21:09:52 +0530

    adding action component

---
+++ src/Actions/actions.js
@@ -0,0 +1,7 @@
+export const INCRIMENT ="INCRIMENT";
+export const DECRIMENT ="DECRIMENT";
+
+export const THEAM_TOGGLE ="THEME_TOGGLE";
+
+export const incriment = () => ({type: INCRIMENT});
+export const decriment = ()=> ({ type: DECRIMENT});
